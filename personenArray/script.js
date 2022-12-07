bd2dab0cf3bae769b3bf3194c52aaebf1f0b310f
Dominik Köberl <dokoe92@gmail.com>
2022-12-07 17:57:34 +0100

    added sort functions

--- personenArray/script.js
+++ personenArray/script.js
@@ -34,18 +34,81 @@ function addPerson() {
     lastNameField.value = "";
     birthYearField.value = "";
     console.log(personen);
-
+    updateTable();
 }
 
 let listBody = document.getElementById("listBody"); 
 
 function updateTable() {
+    listBody.innerHTML = "";
     personen.forEach(element => {
-        let  = document.createElement("p");
-        let firstName = element.getFirstName();
-        let lastName = element.getLastName();
-        let birthYear = element.getAge();
-        e.appendChild(eContent);
-        listBody.appendChild(e);
+        let row = document.createElement("tr");
+        let  firstNameCol= document.createElement("td");
+        let lastNameCol = document.createElement("td");
+        let birthYearCol = document.createElement("td");
+        let firstName = document.createTextNode(element.getFirstName());
+        let lastName = document.createTextNode(element.getLastName());
+        let birthYear = document.createTextNode(element.getAge());
+        firstNameCol.appendChild(firstName);
+        lastNameCol.appendChild(lastName);
+        birthYearCol.appendChild(birthYear);
+        row.appendChild(firstNameCol);
+        row.appendChild(lastNameCol);
+        row.appendChild(birthYearCol);
+
+        listBody.appendChild(row);
     });
 }
+
+function removeAll() {
+    while (personen.length > 0) {
+        personen.pop(); 
+    }
+    updateTable();
+}
+
+function removeLast() {
+    personen.pop();
+    updateTable();
+}
+
+function sortByLastName() {
+    personen.sort(
+        (p1, p2) => {
+
+            if (p1.lastName < p2.lastName) {
+                return -1;
+    }
+            if (p1.lastName > p2.lastName) {
+                return 1;
+            }
+            return 0;
+        }
+
+    )
+    updateTable();
+}
+
+function sortByAge() {
+    personen.sort(
+        (p1,p2) => {
+            if (p1.birthYear < p2.birthYear) {
+                return -1;
+            }
+            if (p1.birthYear > p2.birthYear) {
+                return 1;
+            }
+            return 0;
+        }
+    )
+    updateTable();
+}
+
+
+
+
+
+
+
+
+
